698315ba997cd0e8343bf3ecdcadaf34386d08a3
JackB <volcanicpiggames@gmail.com>
2021-10-05 21:58:28 +0100

    Settings to update on start screen

--- Assets/Scripts/Menu/StartMenu.cs
+++ Assets/Scripts/Menu/StartMenu.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using VolcanicPig.Mobile.Ui; 
-using TMPro; 
+using TMPro;
+using UnityEngine.UI;
+using VolcanicPig;
 
 namespace Game
 {
     public class StartMenu : Menu
     {
-        [SerializeField]  private TextMeshProUGUI titleText; 
+        [SerializeField]  private TextMeshProUGUI titleText;
+
+        [Header("Settings")] 
+        [SerializeField] private CanvasGroup settingsButtonsCanvas; 
+        [SerializeField] private Sprite tickSprite, crossSprite;
+        [SerializeField] private Image hapticsTick, soundTick;
+
+        private Settings _settings; 
         
         public void StartGamePressed()
         {
@@ -18,7 +28,41 @@ namespace Game
         public override void OnMenuOpened()
         {
             base.OnMenuOpened();
+            
+            _settings = GameManager.Instance.Settings;
+            UpdateSettingsSprites();
+            
             if(titleText) titleText.text = $"Level {GameManager.Instance.Level}"; 
+        }
+
+        public void SettingsButtonPressed()
+        {
+            bool enable = !settingsButtonsCanvas.interactable;
+            settingsButtonsCanvas.interactable = enable;
+            settingsButtonsCanvas.blocksRaycasts = enable;
+            settingsButtonsCanvas.DOFade(enable ? 1 : 0, .5f); 
+        }
+        
+        public void ToggleHapticsPressed()
+        {
+            _settings.ToggleHapticsEnabled();
+            hapticsTick.sprite = _settings.HapticsEnabled ? tickSprite : crossSprite; 
+            hapticsTick.color = _settings.HapticsEnabled ? Color.green : Color.red; 
+        }
+
+        public void ToggleSoundPressed()
+        {
+            _settings.ToggleSoundEnabled();
+            soundTick.sprite = _settings.SoundEnabled ? tickSprite : crossSprite;
+            soundTick.color = _settings.SoundEnabled ? Color.green : Color.red; 
+        }
+
+        private void UpdateSettingsSprites()
+        {
+            hapticsTick.sprite = _settings.HapticsEnabled ? tickSprite : crossSprite; 
+            hapticsTick.color = _settings.HapticsEnabled ? Color.green : Color.red; 
+            soundTick.sprite = _settings.SoundEnabled ? tickSprite : crossSprite;
+            soundTick.color = _settings.SoundEnabled ? Color.green : Color.red; 
         }
     }
 }
